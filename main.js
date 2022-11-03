cac062ab2fc3516475ed2c925aea3f9689ce3562
Flávio Félix <fau_felix30@hotmail.com>
2022-11-03 19:46:45 -0300

    Feito a aula 4 refatoração

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ function createGame(player1, hour, player2) {
   `;
 }
 
-
+let delay = -0.3;
 function createCard(date, day, games) {
+  delay = delay + 0.3;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
     <ul>
       ${games}
@@ -20,19 +21,10 @@ function createCard(date, day, games) {
   `;
 }
 
-document.querySelector('#app').innerHTML = `
-  <header>
-  <img src="./assets/logo.svg" alt="LOgo da NLW">
-  </header>
-  <main id="cards">
-    ${createCard("24/11", "quinta",
-      createGame("brazil", "16:00", "serbia")
-    )}
-    ${createCard("28/11", "segunda",
-      createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguai")
-    )}
-    ${createCard("02/12", "sexta",
-      createGame("brazil", "16:00", "cameroon")
-    )}
-  </main>
-`;
+document.querySelector('#cards').innerHTML = 
+    createCard("24/11", "quinta",
+      createGame("brazil", "16:00", "serbia")) +
+    createCard("28/11", "segunda",
+      createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguai")) +
+    createCard("02/12", "sexta",
+      createGame("brazil", "16:00", "cameroon"))
